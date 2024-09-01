8ec8438c21066e65582810e8329b991f88d884ed
cenk-idris <114963355+cenk-idris@users.noreply.github.com>
2024-09-01 15:09:22 +0300

    Update constants.dart comments

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,25 +1,25 @@
 const List<String> stockSymbols = [
-  'BINANCE:SOLUSDT',
-  'BINANCE:BTCUSDT',
-  'BINANCE:XTZUSDT',
+  'BINANCE:SOLUSDT', // Solana to USDT trading pair on Binance
+  'BINANCE:BTCUSDT', // Bitcoin to USDT trading pair on Binance
+  'BINANCE:XTZUSDT', // Tezos to USDT trading pair on Binance
   'AAPL', // Apple Inc. DONE
   'MSFT', // Microsoft Corporation
-  'NVDA', // Amazon.com Inc.
+  'NVDA', // NVIDIA Corporation
   'GOOGL', // Alphabet Inc. Class A
-  'AMZN', // Facebook, Inc.
-  'META', // Tesla, Inc.
+  'AMZN', // Amazon.com, Inc.
+  'META', // Meta Platforms, Inc. (formerly Facebook, Inc.)
   'BRK.B', // Berkshire Hathaway Inc. Class B
   'JPM', // JPMorgan Chase & Co.
   'JNJ', // Johnson & Johnson
   'V', // Visa Inc.
   'WMT', // Walmart Inc.
-  'PG', // Procter & Gamble Co.
+  'PG', // The Procter & Gamble Company
   'MA', // Mastercard Incorporated
-  'NFLX', // TSMC
-  'PLTR', // The Home Depot, Inc.
-  'AMD', // The Walt Disney Company
-  'CVX', // PayPal Holdings, Inc.
-  'TSLA', // UnitedHealth Group Incorporated
-  'IBM', // Verizon Communications Inc.
-  'ORCL', // The Coca-Cola Company
+  'NFLX', // Netflix, Inc.
+  'PLTR', // Palantir Technologies Inc.
+  'AMD', // Advanced Micro Devices, Inc.
+  'CVX', // Chevron Corporation
+  'TSLA', // Tesla, Inc.
+  'IBM', // International Business Machines Corporation (IBM)
+  'ORCL', // Oracle Corporation
 ];
